29ee70deb67bd2d6a344c9f248fa1d24cb6217e2
Stephi <zzt3ph4ni3_3@yahoo.es>
2023-05-03 22:04:00 -0600

    Empezamos de nuevo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,17 @@
 package org.example;
 
+import javax.swing.*;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
 
-    hola
+    String saludo;
+    saludo="";
+
+        JOptionPane.showInputDialog("Hola Chris");
+        System.out.println();
 
 
 
